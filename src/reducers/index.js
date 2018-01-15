d17269acf8bacc2308eeadf0dfb0f655574a273f
zgreathouse <greathouse.zachary@gmail.com>
2018-01-14 21:28:53 -0800

    wire up the authReducer to the rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,7 +3,7 @@ import { combineReducers } from 'redux';
 import authReducer from './authReducer';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  auth: authReducer
 });
 
 export default rootReducer;
