c4513b7c5e534a51ec66885dcc9c68a07a8a75cd
zgreathouse <greathouse.zachary@gmail.com>
2018-01-14 22:18:49 -0800

    implement authentication when component is about to mount and when it is about to update

--- src/components/HigherOrderComponents/require_authentication.js
+++ src/components/HigherOrderComponents/require_authentication.js
@@ -3,6 +3,22 @@ import { connect } from 'react-redux';
 
 const requireAuth = (ComposedComponent) => {
   class Authentication extends Component {
+    static contextTypes = {
+      router: React.PropTypes.object
+    }
+
+    componentWillMount() {
+      if(!this.props.authenticated) {
+        this.context.router.push('/');
+      }
+    }
+
+    componentWillUpdate(nextProps) {
+      if(!nextProps.authenticated) {
+        this.context.router.push('/');
+      }
+    }
+
     render() {
       return <ComposedComponent {...this.props} />
     }
